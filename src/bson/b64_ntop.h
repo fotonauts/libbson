342663ea5f5a0d722262c2feaa1459f6e548849a
Jérôme Lebel <jerome@fotonauts.com>
2014-10-08 19:52:38 +0200

    b64_ntop should return ssize_t and not size_t

--- src/bson/b64_ntop.h
+++ src/bson/b64_ntop.h
@@ -112,7 +112,7 @@ static const char Pad64 = '=';
  *    characters followed by one "=" padding character.
  */
 
-static size_t
+static ssize_t
 b64_ntop (uint8_t const *src,
           size_t         srclength,
           char          *target,
